8df19e4dc8f786fa1a770a1389bed2ad7302b723
Kian <kian.info78@gmail.com>
2020-06-11 03:28:24 +1000

    layout

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React from  'react'
+import { Component } from "react";
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <Aux>
+
+           
+            <div>Burger </div>
+            <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder
